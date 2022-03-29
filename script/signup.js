11c5ce0aaebbffc2117c00fe1b7e347b4f69b83a
Sahil-pillania <sahilpillania88@gmail.com>
2022-03-29 13:06:21 +0530

    Commit

--- script/signup.js
+++ script/signup.js
@@ -1,1 +1,17 @@
 //console.log("signup javascript");
+const form = document.querySelector(".signup form");
+const continueBtn = form.querySelector(".button input");
+
+form.onsubmit = (e)=>{
+    e.preventDefault(); 
+}
+
+continueBtn.onclick = ()=>{
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/signup.php", true);
+    xhr.onload = ()=> {
+
+    }
+    xhr.send();
+}
