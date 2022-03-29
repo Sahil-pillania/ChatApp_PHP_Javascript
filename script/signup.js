18b019ce3dcdd6ff61f8b6eaedbc5a638d4c1ac0
Sahil-pillania <sahilpillania88@gmail.com>
2022-03-29 21:30:27 +0530

    Updated

--- script/signup.js
+++ script/signup.js
@@ -11,7 +11,12 @@ continueBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/signup.php", true);
     xhr.onload = ()=> {
-
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                console.log(data);
+            }
+        }
     }
     xhr.send();
 }
