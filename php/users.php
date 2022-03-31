aedaae986a896f1c79a5bc65ad1bb8d74ec0bbeb
Sahil-pillania <sahilpillania88@gmail.com>
2022-03-31 12:23:56 +0530

    Updated

--- php/users.php
+++ php/users.php
@@ -7,19 +7,20 @@ $output = "";
 if(mysqli_num_rows($sql) == 1){
     $output .= "No users are online.";
 }elseif(mysqli_num_rows($sql) > 0){
-    while($row = mysqli_fetch_assoc($sql)){
-        $output .= '
-        <a href="#">
-        <div class="content">
-          <img src="../php/images/'. $row['img'] . '" alt="" />
-          <div class="details">
-            <span>'. $row['fname']. " " . $row['lname'].'</span>
-            <p>this is test message</p>
-          </div>
-        </div>
-        <div class="status-dot"><i class="fas fa-circle"></i></div>
-      </a> ';
-    }
+    // while($row = mysqli_fetch_assoc($sql)){
+    //     $output .= '
+    //     <a href="#">
+    //     <div class="content">
+    //       <img src="../php/images/'. $row['img'] . '" alt="" />
+    //       <div class="details">
+    //         <span>'. $row['fname']. " " . $row['lname'].'</span>
+    //         <p>this is test message</p>
+    //       </div>
+    //     </div>
+    //     <div class="status-dot"><i class="fas fa-circle"></i></div>
+    //   </a> ';
+    // }
+    include "while.php";
 
 }
   echo $output;   
--- script/users.js
+++ script/users.js
@@ -9,6 +9,25 @@ searchBtn.onclick = ()=>{
     searchBtn.classList.toggle('active');
 }
 
+searchBar.onkeyup = ()=>{
+    let searchTerm = searchBar.value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "../php/search.php", true);
+    xhr.onload = ()=> {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                //console.log(data);
+                usersList.innerHTML = data;
+               
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
+}
+
 setInterval(() => {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "../php/users.php", true);
